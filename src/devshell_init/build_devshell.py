6510ed3b799a233b667946f974e4dac0d9cf984c
Jeremy Fleischman <jeremyfleischman@gmail.com>
2026-04-03 10:56:00 -0700

    More heuristics!

--- src/devshell_init/build_devshell.py
+++ src/devshell_init/build_devshell.py
@@ -2,21 +2,71 @@
 # build a devshell for projects.
 
 from pathlib import Path
+from typing import Callable
+
+MaybeDevshell = dict[Path, list[str]] | None
+
+ENVRC = Path(".envrc")
 
 
-def build_devshell() -> dict[Path, list[str]] | None:
+DEVSHELL_BUILDERS: list[Callable[[], MaybeDevshell]] = []
+
+
+def devshell_builder(builder: Callable[[], MaybeDevshell]):
+    """
+    Decorator to mark a function as a devshell builder.
+    Kind of funky, but the order in which this is called affects
+    the priority of the various devshell builders.
+    """
+    DEVSHELL_BUILDERS.append(builder)
+    return builder
+
+
+def build_devshell() -> MaybeDevshell:
     """
     Return a devshell for the current directory.
     A devshell is a mapping of path to lines that should be in those files.
     """
+    for devshell_builder in DEVSHELL_BUILDERS:
+        devshell = devshell_builder()
+        if devshell is not None:
+            return devshell
 
-    envrc = Path(".envrc")
-    env_cmds: list[str] = []
+    return None
+
+
+@devshell_builder
+def maybe_flake() -> MaybeDevshell:  # pragma: no cover
     if Path("flake.nix").exists():
-        env_cmds.append("use flake")
-    else:
-        return None
+        return {
+            ENVRC: ["use flake"],
+        }
 
-    return {
-        envrc: env_cmds,
-    }
+    return None
+
+
+@devshell_builder
+def maybe_go() -> MaybeDevshell:  # pragma: no cover
+    if Path("go.mod").exists():
+        return {
+            ENVRC: ["use nix -p go"],
+        }
+
+    return None
+
+
+@devshell_builder
+def maybe_python() -> MaybeDevshell:  # pragma: no cover
+    if (
+        Path("pyproject.toml").exists()
+        or Path("setup.py").exists()
+        or Path("requirements.txt").exists()
+    ):
+        return {
+            ENVRC: [
+                "use nix -p python3",
+                "layout python",
+            ],
+        }
+
+    return None
