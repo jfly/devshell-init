a8978ce6a62735afbf4ce98274fcc6a8b3568405
Jeremy Fleischman <jeremyfleischman@gmail.com>
2026-04-03 12:15:30 -0700

    Include shell completions
    Urg

--- nix/uv2nix.nix
+++ nix/uv2nix.nix
@@ -199,10 +199,36 @@ in
         };
 
         packages.default = self'.packages.${project.pname};
-        packages.${project.pname} = mkApplication {
-          venv = pythonSet.mkVirtualEnv "application-env" workspace.deps.default;
-          package = project;
-        };
+
+        # This is an awkward dance to generate shell completions. See
+        # <https://github.com/pyproject-nix/pyproject.nix/issues/429>.
+        packages.${project.pname} =
+          let
+            package = (
+              mkApplication {
+                venv = pythonSet.mkVirtualEnv "application-env" workspace.deps.default;
+                package = project;
+              }
+            );
+            cmd = package.meta.mainProgram;
+          in
+          pkgs.symlinkJoin {
+            inherit (package) name meta;
+            paths = [
+              package
+            ];
+
+            nativeBuildInputs = [ pkgs.installShellFiles ];
+            postBuild = ''
+              # Typer tries to guess the current shell by default
+              export _TYPER_COMPLETE_TEST_DISABLE_SHELL_DETECTION=1
+
+              installShellCompletion --cmd ${cmd} \
+                --bash <($out/bin/${cmd} --show-completion bash) \
+                --fish <($out/bin/${cmd} --show-completion fish) \
+                --zsh <($out/bin/${cmd} --show-completion zsh)
+            '';
+          };
       };
   };
 }
